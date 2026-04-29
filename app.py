c87852286f36534d09e7610b659b4033c4001360
modo1dodo-cloud <modo1dodo@gmail.com>
2026-04-29 04:45:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ import pandas as pd
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "LootBeck Engine is Live! To search, use /search?name=Elon"
+
 @app.route('/search')
 def search():
     name_query = request.args.get('name', '')
     try:
         df = pd.read_csv('data.csv')
-        # بحث مرن عن الاسم
         results = df[df['name'].str.contains(name_query, case=False, na=False)]
         return jsonify(results.to_dict(orient='records'))
     except Exception as e:
         return jsonify({"error": str(e)})
 
-@app.route('/')
-def home():
-    return "LootBeck is Live! Search now."
-
+# السطر الأهم لـ Vercel
 app = app
